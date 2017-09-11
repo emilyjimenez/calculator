ab8c5459c40b5284fae02248e6faf4a0ce6fa09c
Emily Wells Jimenez and Jessie Bell Waite <emily@>
2017-09-11 13:51:49 -0700

    add function for BMI and convert Farenheit to Celsius

--- js/scripts.js
+++ js/scripts.js
@@ -13,4 +13,20 @@ var divide = function (number1, number2) {
 var number1 = parseInt(prompt("enter a number:"));
 var number2 = parseInt(prompt("enter another number:"));
 
-alert(divide(number1, number2));
+var BMI = function (kg, meters) {
+	return kg/Math.pow(meters,2);
+};
+
+var kg = parseFloat(prompt("enter a weight in kg:"));
+var meters = parseFloat(prompt("enter a height in meters:"));
+
+alert(BMI(kg, meters));
+
+
+var toCelsius = function (farenheit) {
+	return (farenheit - 32) *5/9
+};
+
+var farenheit = parseFloat(prompt("enter temperature in celsius"));
+
+alert(toCelsius(farenheit))
