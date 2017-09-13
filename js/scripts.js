21e3e03d4a6500d910c38be8651f8a51486fc487
Emily Wells Jimenez <emwellspdx@gmail.com>
2017-09-12 21:00:57 -0700

    add calculator UI.

--- js/scripts.js
+++ js/scripts.js
@@ -2,31 +2,34 @@ var add = function(number1, number2) {
 	return number1 + number2;
 };
 
-var subtract = function (number1, number2) {
-	return number1 - number2;
-};
+// var subtract = function (number1, number2) {
+// 	return number1 - number2;
+// };
+//
+// var divide = function (number1, number2) {
+// 	return number1 / number2;
+// };
 
-var divide = function (number1, number2) {
-	return number1 / number2;
-};
+$(document).ready(function() {
+	$("form#add").submit(function(event) {
+		event.preventDefault();
+		var number1 = parseInt($("#add1").val());
+		var number2 = parseInt($("#add2").val());
+		var result = add(number1,number2);
+		$("#output").text(result);
+	});
+});
 
-var number1 = parseInt(prompt("enter a number:"));
-var number2 = parseInt(prompt("enter another number:"));
-
-var BMI = function (kg, meters) {
-	return kg/Math.pow(meters,2);
-};
-
-var kg = parseFloat(prompt("enter a weight in kg:"));
-var meters = parseFloat(prompt("enter a height in meters:"));
-
-alert(BMI(kg, meters));
+// var BMI = function (kg, meters) {
+// 	return kg/Math.pow(meters,2);
+// };
+//
+// var kg = parseFloat(prompt("enter a weight in kg:"));
+// var meters = parseFloat(prompt("enter a height in meters:"));
 
 
-var toCelsius = function (farenheit) {
-	return (farenheit - 32) *5/9
-};
+// var toCelsius = function (farenheit) {
+// 	return (farenheit - 32) *5/9
+// };
 
-var farenheit = parseFloat(prompt("enter temperature in celsius"));
-
-alert(toCelsius(farenheit))
+// var farenheit = parseFloat(prompt("enter temperature in celsius"));
